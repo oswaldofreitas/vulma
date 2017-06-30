42780142def41db6a80b80095404e6c917a19332
Oswaldo Freitas <oswaldo.milet@gmail.com>
2017-06-30 18:33:37 -0300

    add radio size option

--- components/forms/radio/radio.vue
+++ components/forms/radio/radio.vue
@@ -1,17 +1,32 @@
 <template lang='pug'>
   .field
-    label.label(v-if='label') {{label}}
-    .control
-      label.radio(v-for='(row, index) in src', :key='index')
-        input(type='radio', :name='`radio${_uid}`', @change='onChange(row)', :checked='getValue(row) === model')
-        |  {{getText(row)}}
+    .field-label(:class='sizeClass')
+      label.label {{label}}
+    .field-body
+      .field.is-narrow
+        .control
+          label.radio(v-for='(row, index) in src', :key='index', :class='sizeClass')
+            input(type='radio', :name='`radio${_uid}`', @change='onChange(row)', :checked='getValue(row) === model')
+            |  {{getText(row)}}
 </template>
+
+<style lang='sass' scoped>
+  .radio
+    &.is-small
+      font-size: 0.75rem
+    &.is-medium
+      font-size: 1.25rem
+    &.is-large
+      font-size: 1.5rem
+</style>
 
 <script>
 import { deepValue } from '../../../utils/object'
+import sizeMixin from '../../../mixins/size'
 
 export default {
   name: 'vulma-radio',
+  mixins: [sizeMixin],
   model: {
     prop: 'selectedValue'
   },
